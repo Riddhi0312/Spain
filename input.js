cdae77885bbdfe6f94f6419295018472d2793215
Riddhi0312 <151597147+Riddhi0312@users.noreply.github.com>
2023-11-21 08:49:20 +0530

    Add files via upload

---
+++ input.js
@@ -0,0 +1,16 @@
+﻿﻿function handleSubmit () {
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const email = document.getElementById('email').value;
+
+    // to set into local storage
+    /* localStorage.setItem("NAME", name);
+    localStorage.setItem("SURNAME", surname);
+    localStorage.setItem("SURNAME", surname);
+    localStorage.setItem("EMAIL", email); */
+    
+    sessionStorage.setItem("NAME", name);
+    sessionStorage.setItem("SURNAME", surname);
+
+    return;
+}
